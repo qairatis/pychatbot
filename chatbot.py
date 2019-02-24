c2c00156b471603136b803be79bac275499d1189
qrts <kayrat.is@gmaqil.com>
2019-02-24 22:30:00 +0600

    Entity extraction with regex

--- chatbot.py
+++ chatbot.py
@@ -40,15 +40,29 @@ def match_intent(message):
     return matched_intent
 
 
-# Define a respond function
+# Define find_name()
+def find_name(message):
+    name = None
+    # Create a pattern for checking if the keywords occur
+    name_keyword = re.compile(r"name|call", re.UNICODE)
+    # Create a pattern for finding capitalized words
+    name_pattern = re.compile(r"\b[A-Z]{1}[a-z]*\b", re.UNICODE)
+    if name_keyword.search(message):
+        # Get the matching words in the string
+        name_words = name_pattern.findall(message)
+        if len(name_words) > 0:
+            # Return the name if the keywords are present
+            name = ' '.join(name_words)
+    return name
+
+# Define respond()
 def respond(message):
-    # Call the match_intent function
-    intent = match_intent(message)
-    # Fall back to the default response
-    key = "default"
-    if intent in responses:
-        key = intent
-    return responses[key]
+    # Find the name
+    name = find_name(message)
+    if name is None:
+        return "Hi there!"
+    else:
+        return "Hello, {0}!".format(name)
 
 
 # Define a function that sends a message to the bot: send_message
@@ -97,6 +111,6 @@ def replace_pronouns(message):
 
 
 # Send messages
-send_message("hello!")
-send_message("bye byeee")
-send_message("thanks very much!")
+send_message("my name is David Copperfield")
+send_message("call me Ishmael")
+send_message("People call me Cassandra")
