89344861ad2e413ef2adfa9732dbafb864041656
qrts <kayrat.is@gmaqil.com>
2019-03-24 12:46:29 +0600

    Data-efficient entity recognition

--- chatbot.py
+++ chatbot.py
@@ -197,3 +197,27 @@ interpreter = trainer.train(training_data)
 
 # Try it out
 print(interpreter.parse("I'm looking for a Mexican restaurant in the North of town"))
+
+
+args = {"pipeline": [
+    {"name": "nlp_spacy"},
+    {"name": "tokenizer_spacy"},
+    {"name": "ner_crf"}
+]}
+
+# Create a config that uses this pipeline
+config = RasaNLUModelConfig(args)
+
+# Create a trainer that uses this config
+trainer = Trainer(config)
+
+# Load the training data
+training_data = load_data("./restaurants.json")
+
+# Create an interpreter by training the model
+interpreter = trainer.train(training_data)
+
+# Parse some messages
+print(interpreter.parse("show me Chinese food in the centre of town"))
+print(interpreter.parse("I want an Indian restaurant in the west"))
+print(interpreter.parse("are there any good pizza places in the center?"))
